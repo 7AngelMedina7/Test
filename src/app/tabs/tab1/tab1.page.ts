ff1af785029b20729698258ddfe79289a07b0979
7AngelMedina7 <101835095+7AngelMedina7@users.noreply.github.com>
2022-04-14 19:51:40 -0500

    añadir carrusel & ubicacion
    preparando para exportar apk

--- src/app/tabs/tab1/tab1.page.ts
+++ src/app/tabs/tab1/tab1.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { DarkModeService } from 'src/app/services/dark-mode.service';
+import { LocationService } from 'src/app/services/location.service';
 
 @Component({
   selector: 'app-tab1',
@@ -8,11 +9,22 @@ import { DarkModeService } from 'src/app/services/dark-mode.service';
 })
 export class Tab1Page implements OnInit {
   
-  constructor(private modoNoche: DarkModeService) { }
+  constructor(private modoNoche: DarkModeService, private locationService: LocationService) { }
   nightModeisOn:boolean;
   ngOnInit() {
     this.nightModeisOn=this.modoNoche.darkMode;
     console.log(this.nightModeisOn)
     console.log(this.modoNoche.darkMode);
+    this.getLocation()
   }
+  lati
+  long
+  getLocation() {
+    this.locationService.getPosition().then(pos => {
+        this.lati = pos.lat;
+        this.long = pos.lng;
+        console.log(this.lati, this.long);
+        
+    });
+}
 }
